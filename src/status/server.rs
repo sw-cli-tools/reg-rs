f8b8ba40d0aef5400ecdcab4961c70e1cf72aadc
Mike Wright <wrightmikea@gmail.com>
2020-02-21 16:17:42 -0800

    simplify code, add failures with differences and passes to status

--- src/status/server.rs
+++ src/status/server.rs
@@ -9,110 +9,30 @@ use crate::finder;
 use crate::status::views::status;
 use crate::time;
 
+lazy_static! {
+    static ref STATE_DATA: Mutex<StateData> = Mutex::new(StateData {
+        pattern: "".to_string(),
+        runs: vec![],
+    });
+}
+
 #[derive(Debug)]
 struct StateData {
-    views: Vec<App>, 
+    pattern: String,
+    runs: Vec<TestDetails>,
 }
 
-lazy_static!{
-    static ref STATE_DATA: Mutex<StateData> = Mutex::new(
-        StateData {
-            views: vec![],
-        });
+#[derive(Debug, Serialize)]
+pub struct TestDetails {
+    pub created: String,
+    pub diffs: Option<Vec<String>>,
+    pub last_ran: Option<String>,
+    pub name: String,
 }
 
-#[derive(Debug)]
-struct App {
-    fail_count: u32,
-    failed_test_names: Vec<String>,
-    no_failed_tests: bool,
-    no_not_yet_run_tests: bool,
-    no_passed_tests: bool,
-    not_yet_run_count: u32,
-    not_yet_run_test_names: Vec<String>,
-    pass_count: u32,
-    passed_test_names: Vec<String>,
-    test_count: u32,
-    test_pattern: Box<String>,
-}
-
-impl App {
-    fn new(
-        fail_count: u32,
-        failed_test_names: Vec<String>,
-        no_failed_tests: bool,
-        no_not_yet_run_tests: bool,
-        no_passed_tests: bool,
-        not_yet_run_count: u32,
-        not_yet_run_test_names: Vec<String>,
-        pass_count: u32,
-        passed_test_names: Vec<String>,
-        test_count: u32,
-        test_pattern: &str) -> Self {
-        App {
-            fail_count,
-            failed_test_names,
-            no_failed_tests,
-            no_not_yet_run_tests,
-            no_passed_tests,
-            not_yet_run_count,
-            not_yet_run_test_names,
-            pass_count,
-            passed_test_names,
-            test_count,
-            test_pattern: Box::new(test_pattern.to_string()),
-        }
-    }
-}
-
-pub fn start(config: &config::Config
-) -> Result<(), Box<dyn std::error::Error>> {
-    md!(&config);
+pub fn start(config: &config::Config) -> Result<(), Box<dyn std::error::Error>> {
     let status_port = config.status_port();
-    md!(status_port);
-    let test_pattern = config.extract_pattern();
-    md!(&test_pattern);
-    let test_names = finder::discover(&config)?;
-    let test_count = *&test_names.found.len() as u32;
-    let mut failed_test_names = vec![];
-    let mut passed_test_names = vec![];
-    let mut not_yet_run_test_names = vec![];
-    for test_name in &test_names.found {
-        let latest_results_table_count = db::latest_results_table_count(&test_name)?;
-        if latest_results_table_count == 0 {
-            not_yet_run_test_names.push(format!("{}", &test_name));
-        } else {
-            let difference_count = db::count_differences(&test_name)?;
-            md!(difference_count);
-            if difference_count > 0 {
-                failed_test_names.push(format!("{}", &test_name));
-            } else {
-                passed_test_names.push(format!("{}", &test_name));
-            }
-        }
-    }
-    let fail_count = failed_test_names.len() as u32;
-    let no_failed_tests = 0 == *&failed_test_names.len();
-    let not_yet_run_count = not_yet_run_test_names.len() as u32;
-    let no_not_yet_run_tests = 0 == *&not_yet_run_test_names.len();
-    let pass_count = passed_test_names.len() as u32;
-    let no_passed_tests = 0 == *&passed_test_names.len();
-    let app = App::new(
-        fail_count,
-        failed_test_names,
-        no_failed_tests,
-        no_not_yet_run_tests,
-        no_passed_tests,
-        not_yet_run_count,
-        not_yet_run_test_names,
-        pass_count,
-        passed_test_names,
-        test_count,
-        test_pattern);
-    {
-        let mut state_data = STATE_DATA.lock().unwrap();
-        state_data.views.push(app); // mutate state before handling requests
-    } // unlock STATE_DATA here, otherwise serve_status_view hangs waiting for lock
+    set_test_runs(&config)?;
     let addr = format!("localhost:{}", status_port);
     println!("Listening at {}.  Ctrl-C to terminate server", addr);
     gotham::start(addr, || Ok(serve_status_view)); // loops until Ctrl-C kills process
@@ -120,23 +40,103 @@ pub fn start(config: &config::Config
 }
 
 fn serve_status_view(state: State) -> (State, (mime::Mime, String)) {
-    let view_state = &STATE_DATA.lock().unwrap().views[0];
+    let state_data = &STATE_DATA.lock().unwrap();
+    let mut failed_test_names = vec![];
+    let mut not_yet_run_test_names = vec![];
+    let mut passed_test_names = vec![];
+
+    let mut copied_runs = vec![];
+    for run in &state_data.runs {
+        copied_runs.push(TestDetails {
+            created: run.created.clone(),
+            diffs: run.diffs.clone(),
+            last_ran: run.last_ran.clone(),
+            name: run.name.clone(),
+        });
+        if run.last_ran.is_none() {
+            not_yet_run_test_names.push(run.name.clone());
+        } else if run.diffs.is_none() {
+            passed_test_names.push(run.name.clone());
+        } else {
+            failed_test_names.push(run.name.clone());
+        }
+    }
     let status_view = status::render(&status::StatusViewContext::new(
-        view_state.fail_count,
-        view_state.failed_test_names.clone(),
-        view_state.no_failed_tests,
-        view_state.no_not_yet_run_tests,
-        view_state.no_passed_tests,
-        view_state.not_yet_run_count,
-        view_state.not_yet_run_test_names.clone(),
-        view_state.pass_count,
-        view_state.passed_test_names.clone(),
+        failed_test_names.len() as u32,
+        failed_test_names.len() == 0 as usize,
+        not_yet_run_test_names.len() == 0 as usize,
+        passed_test_names.len() == 0 as usize,
+        not_yet_run_test_names.len() as u32,
+        passed_test_names.len() as u32,
         time::now(),
-        view_state.test_count,
-        view_state.test_pattern.to_string(),
-    )).unwrap();
-    md!(&status_view);
-    
+        state_data.runs.len() as u32,
+        state_data.pattern.to_string(),
+        copied_runs,
+    ))
+    .unwrap();
     let response_str = format!("<div>{}</div>", status_view);
     (state, (mime::TEXT_HTML, response_str.to_string()))
+}
+
+fn set_test_runs(config: &config::Config) -> Result<(), Box<dyn std::error::Error>> {
+    let mut test_runs = vec![];
+    let test_names = finder::discover(&config)?;
+    for test_name in &test_names.found {
+        let original_result = db::read_original_results(&test_name)?;
+        let latest_results_table_count = db::latest_results_table_count(&test_name)?;
+        if latest_results_table_count == 0 {
+            test_runs.push(TestDetails {
+                created: original_result.time_created,
+                diffs: None,
+                name: test_name.to_string(),
+                last_ran: None,
+            });
+        } else {
+            let latest_result = db::read_latest_results(&test_name)?;
+            let difference_count = db::count_differences(&test_name)?;
+            if difference_count > 0 {
+                test_runs.push(TestDetails {
+                    created: original_result.time_created,
+                    diffs: Some(get_diffs(&test_name)?),
+                    name: test_name.to_string(),
+                    last_ran: Some(latest_result.time_created),
+                });
+            } else {
+                test_runs.push(TestDetails {
+                    created: "TBD".to_string(),
+                    diffs: None,
+                    name: test_name.to_string(),
+                    last_ran: Some(latest_result.time_created),
+                });
+            }
+        }
+    }
+    let mut state_data = STATE_DATA.lock().unwrap();
+    state_data.pattern = config.extract_pattern().to_string();
+    state_data.runs = test_runs;
+    Ok(())
+}
+
+fn get_diffs(test_name: &String) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let differences = db::read_differences(&test_name)?;
+    let mut diffs = vec![];
+    for difference in differences {
+        match difference.0.as_ref() {
+            "5" => continue,
+            "8" => continue,
+            _ => (),
+        }
+        diffs.push(match difference.0.as_ref() {
+            "1" => format!("+ Actual code: {}", difference.1).to_string(),
+            "2" => format!("- Expected code: {}", difference.1).to_string(),
+            "3" => format!("+ Stderr add: {}", difference.1).to_string(),
+            "4" => format!("- Stderr remove: {}", difference.1).to_string(),
+            //   "5" => format!("= Stderr same: {}", difference.1).to_string(),
+            "6" => format!("+ Stdout add: {}", difference.1).to_string(),
+            "7" => format!("- Stdout remove: {}", difference.1).to_string(),
+            //   "8" => format!("= Stdout same: {}", difference.1).to_string(),
+            _ => format!("not yet implemented: {} {}", difference.0, difference.1).to_string(),
+        });
+    }
+    Ok(diffs)
 }
--- src/templates/views.rs
+++ src/templates/views.rs
@@ -1,6 +1,7 @@
 pub static STATUS_VIEW_TEMPLATE: &str = "<small>(server started { server_started })</small>
 <h2>RTT Status Server</h2>
 <br />
+
 <div><h3>Summary</h3>
 <div><span>{ fail_count }<span> failed</div>
 <div>{ not_run_count } not yet run</div>
@@ -8,34 +9,29 @@ pub static STATUS_VIEW_TEMPLATE: &str = "<small>(server started { server_started
 <div>=====</div>
 <div>{ test_count } matched pattern: { test_pattern }</div>
 </div>
+
 <div><h3>Details</h3>
 <div>{{ if no_failed_tests }}
 No Failed Tests{{ else }}
-{ fail_symbol } Failures: {{ for failed_test in failed_test_names }}{ failed_test }{{ if not @last }},&nbsp;{{ endif }}{{ endfor }}{{ endif }}</div>
+{ fail_symbol } Failures: <ul>{{ for run in test_runs }}{{ if run.diffs }}<li>{ run.name }</li>{{ endif }}{{ endfor }}</ul>{{ endif }}</div>
 <div>{{- if no_not_yet_run_tests }}{{ else }}
-{ warn_symbol } Not Yet Run: {{ for not_yet_run_test in not_yet_run_test_names }}{ not_yet_run_test }{{ if not @last }}, {{ endif }}{{ endfor }}{{ endif -}}</div>
+{ warn_symbol } Not Yet Run: <ul>{{ for run in test_runs }}{{ if not run.last_ran }}<li>{ run.name }</li>{{ endif }}{{ endfor }}</ul>{{ endif -}}</div>
 <div>{{ if no_passed_tests }}
 No Passed Tests{{ else }}
-{ pass_symbol } Passed: {{ for passed_test in passed_test_names }}{ passed_test }{{ if not @last }}, {{ endif }}{{ endfor }}{{ endif }}</div>
-</div>
-<div><h3>Failures</h3>
-<div><h4>Differences</h4>
-</div>
-<div><h3>Passes</h3>
-</div>
+{ pass_symbol } Passed: <ul>{{ for run in test_runs }}{{ if run.last_ran }}{{ if not run.diffs }}<li>{ run.name }</li>{{ endif }}{{ endif }}{{ endfor }}</ul>{{ endif }}</div>
+
+<div><h3>Failures</h3><ul>
+{{ for run in test_runs }}{{ if run.diffs }}
+<li>{ fail_symbol } { run.name } - created: { run.created }, failed: { run.last_ran }
+
+<div><p>Differences</p><ul>
+  {{for diff in run.diffs }}
+   <li>{ diff }</li>{{ endfor }}</ul>
+{{ endif }}<br /></li>{{ endfor }}</ul></div>
+
+<div><h3>Passes</h3><ul>
+{{ for run in test_runs }}{{ if not run.diffs }}{{ if run.last_ran }}
+<li>{ pass_symbol } { run.name } - created: { run.created }, last ran: { run.last_ran }</li>
+{{ endif }}{{ endif }}{{ endfor }}</ul></div>
 
 ";
-
-// see mono-rust/tinytemplate2
-
-// <div>{{- for failed_test_name in failed_test_names -}}
-// <div>{ fail_symbol } { failed_test_name } - created: { time_created }, failed: { time_last_ran }, differences count: { differences_count }</div>
-
-// <div>{{- for difference in difference_types }}
-// {{- if @first }}, difference types:{{ endif -}}{ required_blank }{ difference }
-// {{- if not @last }},{{ endif -}}{{ endfor -}}</div>
-// </div>
-
-// {{ endfor }}</div>
-
-
