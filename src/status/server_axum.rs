af9581a90b0653ca2e2827bd7f5794ed312b4250
Mike Wright <wrightmikea@gmail.com>
2025-09-10 19:07:26 -0700

    feat: Add skeleton axum server

---
+++ src/status/server_axum.rs
@@ -0,0 +1,18 @@
+use axum::{
+    routing::get,
+    Router,
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
